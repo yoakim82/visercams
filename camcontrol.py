b24215db835024706b3d72c67c1be87dc4ed18be
joakim <mail@yoakim.com>
2024-05-28 17:27:43 +0200

    using picamera

--- camcontrol.py
+++ camcontrol.py
@@ -10,6 +10,7 @@ import datetime
 
 
 def setup_mqtt(broker):
+    print(f"Connecting to broker with id {camID}")
     mq = mqtt.MqttInterface(broker_address=broker, port=1883, username=camID, password=camID)
 
 
